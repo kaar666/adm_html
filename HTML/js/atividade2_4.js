1af5810efb51a7fd3634cdcccbdc061c7d4a1074
kaar666 <kaleblopesgabriel@gmail.com>
2024-09-30 19:23:34 -0300

    capitulo 2

---
+++ HTML/js/atividade2_4.js
@@ -0,0 +1,3 @@
+const resp = document.querySelector("h3") // primeiro elemento h3 da página
+const cor = document.querySelector("#inCor") // elemento com id=inCor
+const lista = document.querySelector(".lista") // elemento da class=lista
