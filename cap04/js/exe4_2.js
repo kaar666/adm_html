7c3aca2e6361974c63f0dcd498c7cec7e0477cde
kaar666 <kaleblopesgabriel@gmail.com>
2024-10-03 21:23:23 -0300

    index arquivo

--- cap04/js/exe4_2.js
+++ cap04/js/exe4_2.js
@@ -2,21 +2,27 @@ function calcular(){
     const nome= document.getElementById("inNome").value;
     const masculino= document.getElementById("inMasculino").checked;
     const altura= document.getElementById("inAltura").value;
-
-    let peso;
-
+ 
+    let peso = masculino ? Math.pow(altura,2) * 22 : Math.pow(altura,2) * 21;
+    
+    /*
     if(masculino){
       peso = 22 * Math.pow(altura, 2)
+     
     }else{
         peso = 21 * Math.pow(altura, 2)       
-    }
-
-    document.querySelector("h3").innerText = 'peso ideal ${peso.tofixed(2)}';
+        alert(4)
+    }*/
     
-    alert()
+    document.querySelector("h3").innerText = `${nome} peso ideal ${peso.toFixed(2)}`;
+    
+    
 
     return false; //  vai impedir o refresh da pagina equivalente ao e.preventDefault()
 
 
+}
 
+function limpar() {
+  document.querySelector("h3").innerText = ""
 }
